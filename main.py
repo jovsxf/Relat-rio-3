f3b3f2a4f0ecac81450d40be08c0cdb698d2bb5a
jovsxf <gioemanuely@gmail.com>
2024-03-11 23:12:35 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from veny.database import Database
+from helper.WriteAJson import writeAJson
+
+db = Database(database="pokedex", collection="pokemons")
+db.resetDatabase()
+
+
+def getPokemonByName(name: str):
+    return db.collection.find({"name": name})
+
+pikachu = getPokemonByName("Pikachu")
+writeAJson(pikachu, "pikachu")
+
+pokemons = db.collection.find({"$or": [{"type":"Fire"},{"weaknesses": "Fire"}]})
+
+tipos = ["Grass", "Poison"]
+pokemons = db.collection.find({ "type": {"$in": tipos}, "next_evolution": {"$exists": True} })
+writeAJson(pokemons, "tipos")
+
+fraquezas = ["Psychic", "Ice"]
+pokemons = db.collection.find({"weaknesses": {"$all": fraquezas}})
+writeAJson(pokemons, "fraquezas")
+
+pokemons = db.collection.find({"spawn_chance": {"$gt": 0.3, "$lt": 0.6}})
+writeAJson(pokemons, "spawn_chance")
